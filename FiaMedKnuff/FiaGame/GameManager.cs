b5ead34327206a0ac07111e873e14fb17867efcc
Jacob <jacobgsdev@gmail.com>
2024-10-09 11:11:53 +0200

    Created FiaGame namespace with classes

---
+++ FiaMedKnuff/FiaGame/GameManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiaMedKnuff.FiaGame {
+    internal static class GameManager {
+        public static Game Game;
+    }
+}
