9e1a92a1af8b6f9869ad04c5822eda1d2e8079eb
henrik-mju23-2 <henrik.n2004@gmail.com>
2024-10-16 11:21:39 +0200

    Resultatpage is shown once a pawn has reached the center

--- FiaMedKnuff/ResultatPage.xaml.cs
+++ FiaMedKnuff/ResultatPage.xaml.cs
@@ -27,11 +27,6 @@ namespace FiaMedKnuff
             this.InitializeComponent();
         }
 
-        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
-        {
-
-        }
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(MainPage));
